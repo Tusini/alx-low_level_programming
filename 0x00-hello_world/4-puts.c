f7e43767a4ba1fac94c54afbe069b76307f1a26c
Tusini <zamasbanesihle@gmail.com>
2023-08-11 13:20:41 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#!/bin/bash stdio.h
+#include <studio.h>
 
 /**
  * main - this is the main function
  *
- * Return : always 0
+ * Return: always 0
  */
 
 
 int main(void)
 {
      puts("\"Programming is like building a multilingual puzzle);    
-     return 00
+     return (0);
 
 }
         
