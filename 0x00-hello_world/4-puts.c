b5adc61d01e0a35f13de1a464b2381924078677d
Tusini <zamasbanesihle@gmail.com>
2023-08-11 13:27:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * Return : always 0
  */
 
 
 int main(void)
 {
      puts("\"Programming is like building a multilingual puzzle);    
-     return (0);
+     return (0)
 
 }
         
