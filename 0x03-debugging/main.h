5f14a2494ad840250db0983b5dad3019c939c1be
Tusini <zamasbanesihle@gmail.com>
2023-08-17 14:38:32 -0700

    debbug

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIH_H
+#include <stdio.h>
+
+void positive_or_negative(inti i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int mouth, int day,int year);
+int convert_day(int mounth, int day);
+
+#endif
