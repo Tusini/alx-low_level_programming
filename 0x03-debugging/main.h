bd0fa2f150747b2449979b9d619d4361c64fb381
Tusini <zamasbanesihle@gmail.com>
2023-08-18 11:07:52 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIH_H
 #include <stdio.h>
 
-void positive_or_negative(inti i);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int mouth, int day,int year);
 int convert_day(int mounth, int day);
